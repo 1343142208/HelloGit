58adab397220d19e920945b2000a317c989ebfc3
liming <870034185@qq.com>
2021-12-07 18:07:29 +0800

    第2个语句

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -3,5 +3,6 @@ public class Student {
     public static void main(String[] args) {
         System.out.println("helloGit");
         System.out.println("憨憨");
+        System.out.println("憨憨2");
     }
 }
